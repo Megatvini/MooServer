e8c4b7882b5dad9c306c39a7a3068709ead50063
glaba13 <glaba13@freeuni.edu.ge>
2017-07-02 01:57:39 +0400

    Add application type

--- src/main/java/ge/mooserver/services/MooService.java
+++ src/main/java/ge/mooserver/services/MooService.java
@@ -1,6 +1,7 @@
 package ge.mooserver.services;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -9,13 +10,15 @@ import java.net.URISyntaxException;
 public class MooService {
     private Farm farm = new Farm();
     @GET
-    public String  get() throws IOException {
+    @Produces(MediaType.APPLICATION_JSON)
+    public String get() throws IOException {
         System.out.println("get");
         return farm.getCows().toString();
     }
 
     @GET
     @Path("{cowId}")
+    @Produces(MediaType.APPLICATION_JSON)
     public String  getByID( @PathParam("cowId") int id) throws IOException {
         System.out.println("get :" + id);
         return farm.getCowInfo(id).toString();
@@ -33,6 +36,7 @@ public class MooService {
 
     @POST
     @Path("update")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response postUpdate(@QueryParam("cowId") int id, @QueryParam("lon") double lat, @QueryParam("lat")double lon) {
         System.out.println("post");
         long time = System.currentTimeMillis();
@@ -48,6 +52,7 @@ public class MooService {
 
     @POST
     @Path("register")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response postReister(@QueryParam("cowId") int id, @QueryParam("name") String name) {
         try {
             farm.registerCow(name, id);
